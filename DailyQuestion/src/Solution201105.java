3efc6f4fdc53a6f74253606c7f0b392f847eecc8
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2020-11-05 23:53:49 +0800

    11/05 daily question

---
+++ DailyQuestion/src/Solution201105.java
@@ -0,0 +1,51 @@
+// 11.05
+// 127 Word Ladder
+
+/*
+    给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+    - 每次转换只能改变一个字母。
+    - 转换过程中的中间单词必须是字典中的单词。
+
+    说明：
+    如果不存在这样的转换序列，返回 0。
+    所有单词具有相同的长度。
+    所有单词只由小写字母组成。
+    字典中不存在重复的单词。
+    你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+
+    Input:
+    beginWord = "hit",
+    endWord = "cog",
+    wordList = ["hot","dot","dog","lot","log","cog"]
+
+    Output: 5
+
+    Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+    return its length 5.
+
+    Input:
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot","dot","dog","lot","log"]
+
+    Output: 0
+
+    Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+
+*/
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution201105 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(!wordList.contains(endWord))return 0;
+        wordList.sort(String::compareTo);
+
+
+
+
+
+    }
+}
