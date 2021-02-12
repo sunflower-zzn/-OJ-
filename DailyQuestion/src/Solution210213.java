179ec175bac894293c539e713efbc56fdb548c70
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2021-02-13 00:51:27 +0800

    2/13 list与数组

---
+++ DailyQuestion/src/Solution210213.java
@@ -0,0 +1,31 @@
+// 2.13
+// 448. Find All Numbers Disappeared in an Array(数组中消失的数字)
+
+/*
+给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+
+输入:
+[4,3,2,7,8,2,3,1]
+
+输出:
+[5,6]
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution210213 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int ans[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[nums[i] - 1]++;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (ans[i] == 0) list.add(i + 1);
+        }
+        return list;
+    }
+}
