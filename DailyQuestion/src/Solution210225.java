1ea5611ce992a3b2ea2176d9219fa6d69ef1e9a0
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2021-02-25 17:23:54 +0800

    2/25 简单题 转置矩阵

---
+++ DailyQuestion/src/Solution210225.java
@@ -0,0 +1,34 @@
+// 2.25
+// 867. Transpose Matrix(转置矩阵)
+
+/*
+给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[[1,4,7],[2,5,8],[3,6,9]]
+
+输入：matrix = [[1,2,3],[4,5,6]]
+输出：[[1,4],[2,5],[3,6]]
+
+提示：
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+*/
+
+public class Solution210225 {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int res[][] = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+}
