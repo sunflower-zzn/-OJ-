fb00415056a5d451e4cc4cf2825eacf51316004c
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2021-03-06 16:02:01 +0800

    3/5 用栈实现队列
    - 输入栈：push
    - 输出栈：将输入栈逆序，peek/pop

---
+++ DailyQuestion/src/Solution210305.java
@@ -0,0 +1,100 @@
+// 3.5
+// 232. Implement Queue using Stacks(用栈实现队列)
+
+/*
+请你仅使用两个栈实现先入先出队列。队列应当支持一般队列的支持的所有操作（push、pop、peek、empty）：
+实现 MyQueue 类：
+void push(int x) 将元素 x 推到队列的末尾
+int pop() 从队列的开头移除并返回元素
+int peek() 返回队列开头的元素
+boolean empty() 如果队列为空，返回 true ；否则，返回 false
+
+说明：
+你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ 
+进阶：
+你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+ 
+输入：
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+输出：
+[null, null, null, 1, 1, false]
+
+解释：
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+*/
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution210305 {
+    class MyQueue {
+        Deque<Integer> queIn;       //输入栈
+        Deque<Integer> queOut;      //输出栈
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyQueue() {
+            queIn = new LinkedList<>();
+            queOut = new LinkedList<>();
+        }
+
+        /**
+         * Push element x to the back of queue.
+         */
+        public void push(int x) {
+            queIn.push(x);
+        }
+
+        /**
+         * Removes the element from in front of queue and returns that element.
+         */
+        public int pop() {
+            if (queOut.isEmpty()) {
+                while (!queIn.isEmpty()) {
+                    queOut.push(queIn.pop());
+                }
+            }
+            return queOut.pop();
+        }
+
+        /**
+         * Get the front element.
+         */
+        public int peek() {
+            if (queOut.isEmpty()) {
+                while (!queIn.isEmpty()) {
+                    queOut.push(queIn.pop());
+                }
+            }
+            return queOut.peek();
+        }
+
+        /**
+         * Returns whether the queue is empty.
+         */
+        public boolean empty() {
+            if (queIn.isEmpty() && queOut.isEmpty()) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.peek();
+     * boolean param_4 = obj.empty();
+     */
+}
