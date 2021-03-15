03176b62056f79d73b0851cc01425163b8ef65c6
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2021-03-15 09:36:16 +0800

    3/14 设计hashmap
    有待补充

---
+++ LeetCode/DailyQuestion/Solution210314_less.java
@@ -0,0 +1,80 @@
+// 3.14
+// 705. Design HashTable(设计hashtable)
+
+/*
+不使用任何内建的哈希表库设计一个哈希映射（HashMap）。
+实现 MyHashMap 类：
+
+MyHashMap() 用空映射初始化对象
+void put(int key, int value) 向 HashMap 插入一个键值对 (key, value) 。如果 key 已经存在于映射中，则更新其对应的值 value 。
+int get(int key) 返回特定的 key 所映射的 value ；如果映射中不包含 key 的映射，返回 -1 。
+void remove(key) 如果映射中存在 key 的映射，则移除 key 和它所对应的 value 。
+
+输入：
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+输出：
+[null, null, null, 1, -1, null, 1, null, -1]
+
+解释：
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
+myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+
+提示：
+0 <= key, value <= 106
+最多调用 104 次 put、get 和 remove 方法
+ 
+进阶：你能否不使用内置的 HashMap 库解决此问题？
+*/
+
+import java.util.Arrays;
+
+public class Solution210314_less {
+    class MyHashMap {
+        int keys[];
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyHashMap() {
+            keys = new int[1000009];
+            Arrays.fill(keys, Integer.MAX_VALUE);
+        }
+
+        /**
+         * value will always be non-negative.
+         */
+        public void put(int key, int value) {
+            keys[key] = value;
+        }
+
+        /**
+         * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+         */
+        public int get(int key) {
+            return keys[key] != Integer.MAX_VALUE ? keys[key] : -1;
+        }
+
+        /**
+         * Removes the mapping of the specified value key if this map contains a mapping for the key
+         */
+        public void remove(int key) {
+            keys[key] = Integer.MAX_VALUE;
+        }
+    }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+}
