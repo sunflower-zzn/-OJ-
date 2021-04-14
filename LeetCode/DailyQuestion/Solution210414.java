aeab90858c308648271ae17aa90c600fdb3f2469
sunflower-zzn <49340834+sunflower-zzn@users.noreply.github.com>
2021-04-15 01:50:56 +0800

    4/14 数据结构：字典树
    数据结构：字典树

---
+++ LeetCode/DailyQuestion/Solution210414.java
@@ -0,0 +1,98 @@
+/**
+ * @Author sunflower_zzn
+ * @Date 2021/4/14 19:56
+ * @Description 208. 实现 Trie (前缀树)
+ * Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+ * 请你实现 Trie 类：
+ * Trie() 初始化前缀树对象。
+ * void insert(String word) 向前缀树中插入字符串 word 。
+ * boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+ * boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ * <p>
+ * 输入
+ * ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+ * [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+ * 输出
+ * [null, null, true, false, true, null, true]
+ * <p>
+ * 解释
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 True
+ * trie.search("app");     // 返回 False
+ * trie.startsWith("app"); // 返回 True
+ * trie.insert("app");
+ * trie.search("app");     // 返回 True
+ * <p>
+ * 提示：
+ * 1 <= word.length, prefix.length <= 2000
+ * word 和 prefix 仅由小写英文字母组成
+ * insert、search 和 startsWith 调用次数 总计 不超过 3 * 104 次
+ */
+public class Solution210414 {
+    class Trie {
+        //字典树，字母映射表，多叉树，前缀树！
+        boolean isEnd;
+        Trie[] next;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public Trie() {
+            isEnd = false;
+            next = new Trie[26];
+        }
+
+        /**
+         * Inserts a word into the trie.
+         */
+        public void insert(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.next[index] == null) {
+                    node.next[index] = new Trie();
+                }
+                node = node.next[index];
+            }
+            node.isEnd = true;
+        }
+
+        /**
+         * Returns if the word is in the trie.
+         */
+        public boolean search(String word) {
+            Trie node = searchPre(word);
+            return node != null && node.isEnd;
+        }
+
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
+        public boolean startsWith(String prefix) {
+            Trie node = searchPre(prefix);
+            return node != null;
+        }
+
+        public Trie searchPre(String pre) {
+            Trie node = this;
+            for (int i = 0; i < pre.length(); i++) {
+                int index = pre.charAt(i) - 'a';
+                if (node.next[index] == null) {
+                    return null;
+                }
+                node = node.next[index];
+            }
+            return node;
+        }
+    }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+}
